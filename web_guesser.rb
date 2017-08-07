48139ff5dffaf56ca39ae1a9d72ca78793d47fd9
RickyAmparo <rickyamparo@gmail.com>
2017-08-07 17:40:52 -0400

    Add colors method

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,18 +3,38 @@ require 'sinatra/reloader'
 
 Secret_Number = rand(100)
 
+def check_color(message)
+  if message.nil?
+    "white"
+  else
+    if message == "That was way too high!"
+      "red"
+    elsif message == "That was way too low!"
+      "red"
+    elsif message == "That was way too low!"
+      "red"
+    elsif message == "You were too high"
+      "red"
+    elsif message == "You were too low"
+      "red"
+    else
+      "green"
+    end
+  end
+end
+
 def check_guess(guess)
   if guess.nil?
     "Please pick a number between 0 and 100"
   else
-    if guess.to_i > Secret_Number
+    if (guess.to_i - 5) > Secret_Number
+      "That was way too high!"
+    elsif (guess.to_i + 5) < Secret_Number
+      "That was way too low!"
+    elsif guess.to_i > Secret_Number
       "You were too high"
     elsif guess.to_i < Secret_Number
       "You were too low"
-    elsif (guess.to_i - 5) > SECRET_NUMBER
-      "That was way too high!"
-    elsif (guess.to_i + 5) < SECRET_NUMBER
-      "That was way too low!"
     elsif guess.to_i == Secret_Number
       "The Secret Number is #{Secret_Number}\nYou got it correct!!!"
     end
@@ -24,5 +44,8 @@ end
 get '/' do
   guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => {:number => Secret_Number, :message => message}
+  color  = check_color(message)
+  erb :index, :locals => {:number => Secret_Number,
+                          :message => message,
+                          :color => color}
 end
