77a7c80dc694bab2123d3323ad51fa8d79341cca
RickyAmparo <rickyamparo@gmail.com>
2017-08-07 17:07:00 -0400

    add too low and too high conditions

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,12 +4,20 @@ require 'sinatra/reloader'
 Secret_Number = rand(100)
 
 def check_guess(guess)
-  if guess.to_i > Secret_Number
-    "You were too high"
-  elsif guess.to_i < Secret_Number
-    "You were too low"
-  elsif guess.to_i == Secret_Number
-    "You got it correct!!!"
+  if guess.nil?
+    "Please pick a number between 0 and 100"
+  else
+    if guess.to_i > Secret_Number
+      "You were too high"
+    elsif guess.to_i < Secret_Number
+      "You were too low"
+    elsif (guess.to_i - 5) > SECRET_NUMBER
+      "That was way too high!"
+    elsif (guess.to_i + 5) < SECRET_NUMBER
+      "That was way too low!"
+    elsif guess.to_i == Secret_Number
+      "The Secret Number is #{Secret_Number}\nYou got it correct!!!"
+    end
   end
 end
 
