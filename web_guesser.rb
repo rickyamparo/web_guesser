358a127988d9721c92a47c1dd1b49ae096ff9ec7
RickyAmparo <rickyamparo@gmail.com>
2017-08-07 15:59:36 -0400

    add check_guess method

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,5 +4,17 @@ require 'sinatra/reloader'
 number = rand(100)
 
 get '/' do
+  guess = params["guess"].to_i
+  message = check_guess(guess)
   erb :index, :locals => {:number => number}
+end
+
+def check_guess(guess)
+  if guess > number
+    "You were too high"
+  elsif guess < number
+    "You were too low"
+  elsif guess == number
+    "That was the number!!"
+  end
 end
